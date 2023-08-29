c6fd65c18a39ec05da059d9161a4ffb5823c51f6
Alexandre Machado <aleaxe7@gmail.com>
2023-08-29 20:22:53 -0300

    Alter delete records policy from delete cascade to restrict mode

--- SalesMVC/Data/SalesMVCContext.cs
+++ SalesMVC/Data/SalesMVCContext.cs
@@ -9,13 +9,23 @@ namespace SalesMVC.Data
 {
     public class SalesMVCContext : DbContext
     {
-        public SalesMVCContext(DbContextOptions<SalesMVCContext> options)
-            : base(options)
-        {
-        }
+        public SalesMVCContext(DbContextOptions<SalesMVCContext> options) : base(options) { }
 
         public DbSet<Department> Department { get; set; }
         public DbSet<Seller> Seller { get; set; }
         public DbSet<SalesRecord> SalesRecord { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Department>()
+            .HasMany(p => p.Sellers)
+            .WithOne(t => t.Department)
+            .OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<Seller>()
+            .HasMany(p => p.Sales)
+            .WithOne(t => t.Seller)
+            .OnDelete(DeleteBehavior.Restrict);
+        }
     }
 }
